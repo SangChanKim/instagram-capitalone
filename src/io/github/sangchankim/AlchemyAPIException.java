3998c0d5757b20d757e45202a9bc2a71bd482646
sangchankim <sck970112@gmail.com>
2015-10-28 12:34:48 -0400

    commited new source code

---
+++ src/io/github/sangchankim/AlchemyAPIException.java
@@ -0,0 +1,12 @@
+package io.github.sangchankim;
+
+public class AlchemyAPIException extends Exception {
+
+    public AlchemyAPIException(String message) {
+        super(message);
+    }
+
+    public AlchemyAPIException(String message, Throwable t) {
+        super(message, t);
+    }
+}
---
+++ src/io/github/sangchankim/Comment.java
@@ -0,0 +1,33 @@
+package io.github.sangchankim;
+
+/**
+ * Represents a comment left on an Instagram media post
+ * @author sangchankim
+ * @version 1.0
+ */
+
+public class Comment {
+    private User author;
+    private String text;
+
+    public Comment(User author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
